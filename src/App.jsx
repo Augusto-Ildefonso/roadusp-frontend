12cdf985791578c2af5423f5932c357f32aa0fd4
Augusto-Ildefonso <afildefonso16@gmail.com>
2025-09-18 23:50:24 -0300

    fix: analytics no package.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './App.css';
 import GraphPage from './pages/GraphPage';
 import HomePage from './pages/HomePage';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path='/graph' element={<GraphPage/>}/>
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path='/graph' element={<GraphPage/>}/>
+        </Routes>
+      </Router>
+      <Analytics />
+    </div>
   );
 }
 
